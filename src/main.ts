068b574177aab4a77fa9982801bc2c9fe41041e3
yohagos <yosef.hagos@googlemail.com>
2024-12-29 09:11:47 +0100

    config and env files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, DomSanitizer } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { MatIconRegistry } from '@angular/material/icon';
+import { ErrorHandler, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DOCUMENT } from '@angular/common';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideCharts(withDefaultRegisterables()),
+    ...appConfig.providers,
+  ]
+})
   .catch((err) => console.error(err));
